80876437a0c638b1c3b39458a9bdd319f133c904
Unknown22 <marcin700@gmail.com>
2015-06-04 20:46:57 +0200

    Dodany komentarz

--- Zadanie 3.py
+++ Zadanie 3.py
@@ -1,3 +1,10 @@
+"""
+W czesci A zostaja wyswietlone wszystkie elementy, w ktorych wystepuja dwie takie same litery obok siebie.
+W czesci B zostaja wyswietlone elementy pogrupowane wedlug dlugosci elementww
+Listy mozna dowolnie edytowac
+
+"""
+
 list1 = ['orange', 'apple', 'banana']
 list2 = ('carrot', 'potato', 'lettuce')
 list3 = {'milk', 'sugar', 'butter', 'flour'}
