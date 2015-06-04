a95c1f836b386c7befebedc5baec5096b4765cc2
Unknown22 <marcin700@gmail.com>
2015-06-04 16:09:33 +0200

    Dodana część B, zadanie w całości rozwiązane

--- Zadanie 3.py
+++ Zadanie 3.py
@@ -6,9 +6,24 @@ list4.extend(list1)
 list4.extend(list2)
 list4.extend(list3)
 
+#A part
+print "Czesc A:"
 for x in list4:
     letter = x[0]
     for y in x[1:]:
         if letter == y:
             print x
         letter = y
+
+#B part
+print "\nCzesc B:"
+dict = {}
+lista_pomocnicza = []
+for x in list4:
+    lenght = len(x)
+    if (dict.has_key(lenght)):
+         lista_pomocnicza = list(dict[lenght])
+    lista_pomocnicza.append(x)
+    dict[lenght] = tuple(lista_pomocnicza)
+    lista_pomocnicza = []
+print dict
